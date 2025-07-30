dc3f00b152ee0e44f85d016fcd01ee44310ab6c2
Youssef-Magdy-19 <youssefostora1155@gmail.com>
2025-07-30 21:53:18 +0300

    latest update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-<<<<<<< HEAD
-
-export default defineConfig({
-  plugins: [react()],
-=======
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
@@ -13,5 +8,4 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
->>>>>>> f534614f674defe12968f1e740397c4b4384db6e
 })
